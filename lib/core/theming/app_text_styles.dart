1b94dbe8ccd3b62c20e74d83297ad54bf91729be
mohamed-yaseen <01221316865m@gmail.com>
2025-04-27 11:34:50 +0300

    Add app-colors and app-text-styles in theming

---
+++ lib/core/theming/app_text_styles.dart
@@ -0,0 +1,65 @@
+import 'package:doc_app/core/theming/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextStyles {
+  static TextStyle font24Black700Weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static TextStyle font32MainBlue700Weight = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.mainBlue,
+  );
+
+  static TextStyle font11Grey400Weight = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey,
+  );
+
+  static TextStyle font16White600Weight = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+
+  static TextStyle font24MainBlue700Weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.mainBlue,
+  );
+
+  static TextStyle font14Grey400Weight = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey,
+  );
+
+  static TextStyle font14Grey500Weight = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.grey,
+  );
+
+  static TextStyle font11Black500Weight = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static TextStyle font11Black400Weight = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static TextStyle font11MainBlue600Weight = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.mainBlue,
+  );
+}
