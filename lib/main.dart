df1568f0a741915dfccd280708c0e26aeec29e2f
mohamed-yaseen <01221316865m@gmail.com>
2025-05-01 10:35:51 +0300

    Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,11 @@
 import 'package:doc_app/core/dependency_injection/di.dart';
 import 'package:doc_app/core/routing/app_router.dart';
-import 'package:doc_app/core/routing/app_routes.dart';
-import 'package:doc_app/core/theming/app_colors.dart';
+import 'package:doc_app/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(appRouter: AppRouter()));
-}
-
-class DocApp extends StatelessWidget {
-  final AppRouter appRouter;
-  const DocApp({super.key, required this.appRouter});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      child: MaterialApp(
-        title: 'Doc App',
-        theme: ThemeData(
-          primaryColor: AppColors.mainBlue,
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        debugShowCheckedModeBanner: false,
-        initialRoute: AppRoutes.onboardingScreen,
-        onGenerateRoute: appRouter.generateRoute,
-      ),
-    );
-  }
 }
