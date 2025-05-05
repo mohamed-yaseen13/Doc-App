bef84656b7b73f0416331952f755630e79b12e2e
mohamed-yaseen <01221316865m@gmail.com>
2025-05-05 07:22:03 +0300

    Fix Auto Incremnet Version

--- android/fastlane/update_version_and_release.rb
+++ android/fastlane/update_version_and_release.rb
@@ -14,8 +14,29 @@ end
 def auto_increment_version
   current = get_current_version
   version_part, build_part = current.split('+')
-  new_build = build_part.to_i + 1
-  new_version = "#{version_part}+#{new_build}"
+  version_numbers = version_part.split('.')
+  major = version_numbers[0].to_i
+  minor = version_numbers[1].to_i
+  patch = version_numbers[2].to_i
+  build = build_part.to_i
+
+  # Increment the patch version when updating the build number
+  patch += 1
+  build = 1  # Reset the build number to 1
+
+  # If patch reaches 10, increment minor and reset patch
+  if patch >= 10
+    patch = 0
+    minor += 1
+  end
+
+  # If minor reaches 10, increment major and reset minor
+  if minor >= 10
+    minor = 0
+    major += 1
+  end
+
+  new_version = "#{major}.#{minor}.#{patch}+#{build}"
 
   lines = File.readlines(PUBSPEC_PATH)
   lines.map! { |line| line.start_with?('version:') ? "version: #{new_version}\n" : line }
